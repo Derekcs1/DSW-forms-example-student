d67a769e7062634c8963f0ab6b3fc980d58fb758
Derekcs1 <31251508+Derekcs1@users.noreply.github.com>
2017-10-10 09:39:09 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,7 +8,7 @@ def render_main():
 
 @app.route("/response")
 def render_response():
-    color = request.args['color']
+    color = request.args['mph']
     #The request object stores information about the request sent to the server.
     #args is a MultiDict (like a dictionary but can have multiple values for the same key)
     #The information in args is visible in the url for the page being requested (ex. .../response?color=blue)
